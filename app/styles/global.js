d04009438e4ce8f8f65a83a8d46ee84e976580be
nmercurio <mercurio.ncl@gmail.com>
2026-02-28 12:44:21 -0600

    updated font weights in global.js

--- app/styles/global.js
+++ app/styles/global.js
@@ -1,11 +1,67 @@
-export const GlobalText = {
-  regular: {
-    fontFamily: 'Nunito-Regular',
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    backgroundColor: '#F5F9FF' 
   },
-  bold: {
-    fontFamily: 'Nunito-Bold',
+  header: { 
+    padding: 25, 
+    backgroundColor: '#00ADEF', 
+    borderBottomLeftRadius: 30, 
+    borderBottomRightRadius: 30 
   },
-  black: {
-    fontFamily: 'Nunito-Black',
+  brandTitle: { 
+    fontSize: 32, 
+    fontFamily: 'Nunito-Black', 
+    color: 'white' 
+  },
+  tagline: { 
+    fontFamily: 'Nunito-Regular', 
+    color: 'white', 
+    opacity: 0.9, 
+    fontSize: 14 
+  },
+  card: { 
+    backgroundColor: 'white', 
+    margin: 15, 
+    padding: 20, 
+    borderRadius: 20, 
+    elevation: 4 
+  },
+  cardTitle: { 
+    fontSize: 18, 
+    fontFamily: 'Nunito-Bold', 
+    color: '#333' 
+  },
+  bpmText: { 
+    fontSize: 48, 
+    fontFamily: 'Nunito-Black', 
+    color: '#00ADEF', 
+    marginTop: 10 
+  },
+  intensityLabel: { 
+    textAlign: 'center', 
+    fontFamily: 'Nunito-Bold', 
+    color: '#00ADEF', 
+    marginTop: 5 
+  },
+  modeText: { 
+    fontSize: 12, 
+    fontFamily: 'Nunito-Bold', 
+    marginTop: 10, 
+    color: '#444' 
+  },
+  safetyCard: { 
+    borderLeftWidth: 8, 
+    borderLeftColor: '#ff4444' 
+  },
+  safetyTitle: { 
+    fontSize: 18, 
+    fontFamily: 'Nunito-Bold', 
+    color: '#ff4444' 
+  },
+  safetySub: { 
+    fontFamily: 'Nunito-Regular', 
+    color: '#666', 
+    fontSize: 13 
   }
-};
+});
